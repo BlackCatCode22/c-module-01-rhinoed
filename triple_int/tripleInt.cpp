e84042f7db7de50e7de1a2b54b1036a58f53cdf8
rhinoed <36555602+rhinoed@users.noreply.github.com>
2025-01-15 19:46:24 -0800

    add working code tripleInt.cpp

--- triple_int/tripleInt.cpp
+++ triple_int/tripleInt.cpp
@@ -1,6 +1,18 @@
+// tripleInt.cpp
+// CIT 66 Spring 2025
+// Created by Mark Edmunds
+// Created on 1-15-25
+// program takes a given integer and triples it.
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int num;
+    // prompt for number
+    std::cout << "Enter a nuber to triple:" << std::endl;
+    // read input
+    std::cin >> num;
+    // send num * 2 to standard out
+    std::cout << "Your number tripled is: " << 3 * num << std::endl;
     return 0;
 }

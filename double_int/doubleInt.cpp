fbb8a77e90a519d4ac4fdbf8aef327e11d1067db
rhinoed <36555602+rhinoed@users.noreply.github.com>
2025-01-15 19:47:51 -0800

    add working code to doubleInt.cpp

--- double_int/doubleInt.cpp
+++ double_int/doubleInt.cpp
@@ -1,6 +1,18 @@
+// doubleInt.cpp
+// CIT 66 Spring 2025
+// Created by Mark Edmunds
+// Created on 1-15-25
+// The program takes a given integer and doubles it.
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int num;
+    // prompt for number
+    std::cout << "Enter a nuber to double:" << std::endl;
+    // read input
+    std::cin >> num;
+    // send num * 2 to standard out
+    std::cout << "Your number doubled is: " << 2 * num << std::endl;
     return 0;
 }

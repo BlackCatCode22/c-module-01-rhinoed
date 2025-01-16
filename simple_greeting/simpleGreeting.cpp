69d3741cc649e59c597b4e8aa3034160c1d3d18b
rhinoed <36555602+rhinoed@users.noreply.github.com>
2025-01-15 19:44:59 -0800

    add working code

--- simple_greeting/simpleGreeting.cpp
+++ simple_greeting/simpleGreeting.cpp
@@ -1,6 +1,27 @@
+// simpleGreeting.cpp
+// CIT 66 Spring 2025
+// Created by Mark Edmunds
+// Created on 1-15-25
+// The program provides a simple greeting to a given user
+
+
+
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // create character array
+    string name;
+    // create salutation
+    const string salutation = "Hello";
+    // prompt for name
+    cout << "What is your name?:" << endl;
+    // read standard in and set name
+    cin >> name;
+    // crate greeting
+    const string greeting = salutation + " " + name + ", " + "welcome to the world of C++";
+    // output to standard out
+    cout << greeting << endl;
     return 0;
 }
